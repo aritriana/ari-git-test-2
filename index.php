4bb1a5f9e88425e1eac3b52cdc07ee05cbdcf58e
aritriana <triana.ari@gmail.com>
2025-07-27 21:47:55 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque error aut quibusdam in, debitis cupiditate doloribus fuga molestias suscipit perferendis cum autem ut vel. Provident voluptas tenetur aspernatur cum in!</p>
+</body>
+</html>
